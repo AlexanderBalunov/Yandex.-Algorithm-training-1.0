8f31e01c2e260fc920bb08181921f6c3ca112ca7
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2025-05-11 06:25:45 +0300

    Решение ( ๑‾̀◡‾́)σ"

--- Lecture 1: "Complexity, testing, special cases"/A. Conditioner/cpp_solution.cpp
+++ Lecture 1: "Complexity, testing, special cases"/A. Conditioner/cpp_solution.cpp
@@ -1,1 +1,34 @@
+#include <cstdint>
+#include <iostream>
 
+int main() {
+    int16_t room, cond;
+    std::cin >> room >> cond;
+    std::string mode;
+    std::cin >> mode;
+    std::cout << (mode == "freeze" ? (room > cond ? cond : room) : 
+                  mode == "heat" ? (cond > room ? cond : room) :
+                  mode == "auto" ? cond : room) << std::endl;
+    return 0;
+}
+
+/*
+    Если расписывать через else-if логику вложенных тернарных операторов:
+    if (mode == "freeze") {
+        if (room > cond) {
+            std::cout << cond << std::endl;
+        } else {
+            std::cout << room << std::endl;
+        }
+    } else if (mode == "heat") {
+        if (cond > room) {
+            std::cout << cond << std::endl;
+        } else {
+            std::cout << room << std::endl;
+        }
+    } else if (mode == "auto") {
+        std::cout << cond << std::endl;
+    } else {
+        std::cout << room << std::endl;
+    }
+*/
